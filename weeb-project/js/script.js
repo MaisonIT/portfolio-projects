53b1e5c75976d6537dc16a539813851e9972cd7f
Mike <98334559+MaisonIT@users.noreply.github.com>
2026-04-21 19:50:10 +0300

    Add files via upload

--- weeb-project/js/script.js
+++ weeb-project/js/script.js
@@ -201,25 +201,3 @@ createApp({
     </div>
    `
 }).mount(`#auth-popup-app`)
-
-// window.addEventListener(`load`, windowLoad)
-// const html = document.documentElement
-
-// function windowLoad() {
-// 	document.addEventListener(`click`, documentActions)
-// 	html.classList.add(`loaded`)
-// }
-// function documentActions(e) {
-// 	const targetElement = e.target;
-
-// 	if (targetElement.closest(`.icon-menu`)) {
-// 		html.classList.toggle(`menu-open`)
-// 	}
-// 	targetElement.closest('.menu__link') && html.classList.contains('menu-open') ? html.classList.remove('menu-open') : null
-// }
-// document.querySelectorAll(`.items-skills__item`).forEach((item, index) => {
-// 	item.style.transitionDelay = `${index * 0.5}s`
-// })
-// document.querySelectorAll(`.reference__item`).forEach((item, index) => {
-// 	item.style.transitionDelay = `${index * 0.2}s`
-// })
